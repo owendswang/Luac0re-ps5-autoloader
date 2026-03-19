8df3f10080e3018085382af82bffaffe58152a49
Gezine <30000519+Gezine@users.noreply.github.com>
2026-03-19 17:07:16 +0900

    Add dlsym test shellcode

---
+++ jit_shellcode/dlsym_test/dlsym.c
@@ -0,0 +1,39 @@
+#include "types.h"
+
+#define LIBKERNEL_HANDLE 0x2001
+#define GADGET_OFFSET    0x31aa9  // call rbx; ret
+
+typedef s32 (*dlsym_t)(s32 handle, const char* sym, void** addr_out);
+
+__attribute__((naked))
+static u64 eboot_wrap(void* gadget, void* fn,
+                      u64 a1, u64 a2, u64 a3,
+                      u64 a4, u64 a5, u64 a6)
+{
+    __asm__ (
+        "mov rbx, rsi\n\t"        // rbx = fn
+        "mov rax, rdi\n\t"        // rax = gadget
+        "mov rdi, rdx\n\t"        // a1
+        "mov rsi, rcx\n\t"        // a2
+        "mov rdx, r8\n\t"         // a3
+        "mov rcx, r9\n\t"         // a4
+        "mov r8,  [rsp + 8]\n\t"  // a5
+        "mov r9,  [rsp + 16]\n\t" // a6
+        "call rax\n\t"
+        "ret"
+    );
+}
+
+__attribute__((section(".text.start")))
+u64 main(u64 EBOOT_BASE, u64 SCE_KERNEL_DLSYM, u64 arg3, u64 arg4, u64 arg5, u64 arg6)
+{
+    void*   gadget = (void*)(EBOOT_BASE + GADGET_OFFSET);
+    dlsym_t dlsym  = (dlsym_t) SCE_KERNEL_DLSYM;
+
+    void* read_addr = 0;
+    eboot_wrap(gadget, (void*)dlsym,
+               LIBKERNEL_HANDLE, (u64)"read", (u64)&read_addr,
+               0, 0, 0);
+
+    return (u64) read_addr;
+}
---
+++ payloads/shellcode_test3.lua
@@ -0,0 +1,15 @@
+
+-- dlsym_test shellcode
+local shellcode = "4883EC184531C94881C7A91A0300BA0120000048C744240800000000488D0D3E0000006A006A004C8D442418E80F000000488B4424184883C428C30F1F4400004889F34889F84889D74889CE4C89C24C89C94C8B4424084C8B4C2410FFD0C30F0B7265616400"
+
+if not sceKernelDlsym then
+    init_dlsym()
+end
+
+write_shellcode(SHELLCODE_BASE, shellcode)
+
+local dlsym_shellcode_test = func_wrap(SHELLCODE_BASE)
+local read_addr = dlsym_shellcode_test(EBOOT_BASE, SCE_KERNEL_DLSYM)
+
+send_notification("read function address in libkernel : ".. to_hex(read_addr))
+
