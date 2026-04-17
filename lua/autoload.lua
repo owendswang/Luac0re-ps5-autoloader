c22cbca514cd1c6410aee9643bea12992fcb5128
owendswang <owendswang@gmail.com>
2026-04-17 15:40:38 +0800

    Fix the version number

--- lua/autoload.lua
+++ lua/autoload.lua
@@ -72,7 +72,7 @@ end
 
 
 function main()
-    send_notification("Autoloader 0.4\nbased on itsPLK/ps5_lua_autoloader")
+    send_notification("Autoloader 0.4\nbased on itsPLK's work")
 
     if not is_jailbroken() then
         ulog("Not jailbroken\nSkipped autoloading payloads")
